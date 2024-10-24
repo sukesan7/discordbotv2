f85f68c77a33e8d33d86029e84f424fb2b08385b
Sukesan <139752315+sukesan7@users.noreply.github.com>
2024-10-24 05:12:15 -0400

    Delete security.py

--- security.py
+++
@@ -1,7 +0,0 @@
-# security.py
-
-DISCORD_TOKEN = ''
-
-# Channel IDs where updates will be posted
-NBA_CHANNEL_ID = 1297005102028161075  # Replace with your NBA injury channel ID
-NFL_CHANNEL_ID = 1297005102028161075  # Replace with your NFL injury channel ID
